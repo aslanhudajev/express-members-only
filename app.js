f5b3be57cfab5e117324ebb2c387c1358b1970a4
Aslambek Hudajev <aslambek.hudajev@gmail.com>
2024-03-25 23:00:05 +0100

    schemas done. creating controllers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 
@@ -10,6 +10,10 @@ await mongoose
 const app = express();
 app.set("view enging", "ejs");
 app.set("views", "./views");
+
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on port " + process.env.PORT)
---
+++ controllers/post.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import expressAsyncHandler from "express-async-handler";
+import Post from "../models/Post";
+
+export const getPost = expressAsyncHandler(async (req, res, next) => {});
